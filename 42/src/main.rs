282c0ece27c62d75689315207515c5e58efa112b
Kara <kara@DESKTOP-JPSV3US.localdomain>
2026-02-20 12:09:00 -0800

    added yellow "Hello World!" functionality to main.rs using VGA, added framework for expanding to custom strings and colours

--- 42/src/main.rs
+++ 42/src/main.rs
@@ -2,9 +2,26 @@
 #![no_main]
 #![allow(unconditional_recursion)]
 
+const VGA_AD: *mut u8 = 0xb8000 as *mut u8;
+const S: &str = "Hello World!";
+const COL: u8 = 0xE;
+
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    loop {}
+pub extern "C" fn _start() -> () {
+    print_vga(S, COL);
+}
+
+pub fn print_vga(s: &str, color_code: u8) {
+    if s.len() <= 24 {
+        let mut offset: usize = 0;
+        for byte in s.bytes() {
+            unsafe {
+                *VGA_AD.add(offset) = byte;
+                *VGA_AD.add(offset + 1) = color_code;
+            }
+            offset += 2;
+        }
+    }
 }
 
 #[panic_handler]
